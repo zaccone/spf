d3f650fcbef05b3ebc31b706ea965bf9c50e7b0a
Marek Denis <marek.denis@gmail.com>
2016-06-29 16:42:09 +0200

    Break too long comments in dns/dns.go

--- dns/dns.go
+++ dns/dns.go
@@ -28,22 +28,27 @@ func checkSPFVersion(spf []string) bool {
 }
 
 // LookupSPF retireves SPF query from domain in question.
-// If also carries out initial validation on whether the TXT record is an SPF record (by comparing the string to a 'v=spf1' value)
-// In the future function should properly handle all known DNS related errors as well as recurively query for SPF records
+// If also carries out initial validation on whether the TXT record is an SPF
+// record (by comparing the string to a 'v=spf1' value)
+// In the future function should properly handle all known DNS related errors
+// as well as recurively query for SPF records
 // TODO(zaccone): Handle typical DNS errors and recusive calls
 func LookupSPF(domain string) ([]string, error) {
 	var spfRecords []string
 	var err error
 	if spfRecords, err = net.LookupTXT(domain); err != nil {
 		/*
-			Note(zaccone): We need to handle DNS related errors in the upper layer, as depending on error type a SPF related
-			result/exception will be raised.
+			Note(zaccone): We need to handle DNS related errors in the upper
+			layer, as depending on error type a SPF related result/exception
+			will be raised.
 		*/
 		return nil, err
 	}
 
 	if checkSPFVersion(spfRecords) == false {
-		return nil, errors.New(strings.Join([]string{"Invalid SPF record: ", strings.Join(spfRecords, " ")}, " "))
+		return nil, errors.New(strings.Join(
+			[]string{"Invalid SPF record: ", strings.Join(spfRecords, " ")},
+			" "))
 	}
 
 	return spfRecords, nil
