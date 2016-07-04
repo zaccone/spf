6e851579ba74b26d33323f092685e10ef86d3300
Marek Denis <marek.denis@gmail.com>
2016-07-04 11:28:13 +0200

    Handle DNS errors.
    Implement logic described in Section 4.3 and 4.4 of RFC 7208.

--- spf/spf.go
+++ spf/spf.go
@@ -55,6 +55,13 @@ func (spf SPFResult) String() string {
 // function can focus on the core part.
 func checkHost(ip net.IP, domain, sender string) (SPFResult, error) {
 
+	/*
+			* As per RFC 7208 Section 4.3:
+			* If the <domain> is malformed (e.g., label longer than 63
+			* characters, zero-length label not at the end, etc.) or is not
+			* a multi-label
+		    * domain name, [...], check_host() immediately returns None
+	*/
 	if !dns.IsDomainName(domain) {
 		return None, nil
 	}
@@ -66,9 +73,32 @@ func checkHost(ip net.IP, domain, sender string) (SPFResult, error) {
 		// as per RFC7208 section 4.4, DNS query errors result in Temperror
 		//result immediately
 		case *net.DNSError:
-			return Temperror, dnsErr
+			/*
+			* As per RFC 7208 Section 4.3:
+			* [...] or if the DNS lookup returns "Name Error" (RCODE 3, also
+			* known as "NXDOMAIN" [RFC2308]), check_host() immediately returns
+			* the result "none".
+			*
+			* Sadly, net.DNSError does not provide RCODE statuses, however
+			* reading it' implementation we can deduct that
+			* DNSError.Err string is set to "no such host" upon RCODE 3.
+			* See
+			* https://github.com/golang/go/blob/master/src/net/dnsclient.go#L43
+			* for the logic implementation and
+			* https://github.com/golang/go/blob/master/src/net/net.go#L547 for
+			* `errNoSuchHost` error definition.
+			*
+			* On the other had, any other RCODE not equal to 0 (success, so no
+			* error) or 3 or timeout occurs, check_host() should return
+			* Temperror which we handle too.
+			 */
+			if dnsErr.(*net.DNSError).Err == "no such host" {
+				return None, nil
+			} else {
+				return Temperror, nil
+			}
 		default:
-			return Permerror, dnsErr
+			return Permerror, nil
 
 		}
 	}
