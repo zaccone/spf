3de20f47df0936aaf1c670d7dc535bf23805d187
Marek Denis <marek.denis+zaccone@gmail.com>
2016-12-29 18:19:43 +0000

    Small progress.

--- tests/main.go
+++ tests/main.go
@@ -4,17 +4,20 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"os"
 )
 
 type Zonedata map[string][]map[string]string
 
 type Test struct {
-	Helo     string
-	Host     string
-	Mailfrom string
-	Result   string
-	Receiver string
-	Header   string
+	Helo        string
+	Host        string
+	Mailfrom    string
+	Result      string
+	Receiver    string
+	Header      string
+	Comment     string `json:",omitempty"`
+	Explanation string `json:",omitempty"`
 }
 
 type Tests map[string]Test
@@ -27,8 +30,37 @@ type TestCase struct {
 
 type JSONTest []TestCase
 
+func runDNS(zone *Zonedata) error {
+	fmt.Printf("Running DNS Server for Zone %+v\n", *zone)
+	for host, records := range *zone {
+		for _, record := range records {
+			for t, v := range record {
+				fmt.Printf("%s 0 IN %s %s\n", host, t, v)
+			}
+		}
+	}
+	return nil
+}
+
+func runTests(tc *JSONTest) {
+	for _, testcase := range *tc {
+		runDNS(&testcase.Zonedata)
+
+		for k, v := range testcase.Tests {
+			fmt.Printf("Running test %s spf.CheckHost(ip=%s, domain=%s, sender=%s)\n", k, v.Host, v.Helo, v.Mailfrom)
+
+		}
+	}
+}
+
 func main() {
-	const fname = "/tmp/asd.json"
+
+	if len(os.Args) < 2 {
+		fmt.Printf("Usage %s <file.json>", os.Args[0])
+		os.Exit(1)
+	}
+	fname := os.Args[1]
+
 	fh, err := ioutil.ReadFile(fname)
 	if err != nil {
 		panic(err)
@@ -40,10 +72,6 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("%+v\n", tc)
-
-	for _, test := range tc {
-		fmt.Printf("val: %+v\n", test)
-	}
+	runTests(&tc)
 
 }
