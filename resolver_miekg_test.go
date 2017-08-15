c404f8f8af7067efc8dd533af945796b8be0b911
Dmitry Motylev <dmitry@redsift.io>
2017-08-15 11:52:35 +0100

    tests for multiline txt lookup by miekgdns added

--- resolver_miekg_test.go
+++ resolver_miekg_test.go
@@ -1,10 +1,52 @@
 package spf
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/miekg/dns"
+)
 
 func TestMiekgDNSResolver(t *testing.T) {
 	_, e := NewMiekgDNSResolver("8.8.8.8") // invalid TCP address, no port specified
 	if e == nil {
 		t.Errorf(`want "address 8.8.8.8: missing port in address"`)
+	}
+}
+
+func TestMiekgDNSResolver_LookupTXTStrict_Multiline(t *testing.T) {
+	dns.HandleFunc("multiline.test.", zone(map[uint16][]string{
+		dns.TypeTXT: {
+			`multiline.test. 0 IN TXT "v=spf1 ip4:10.0.0.1 ip4:10.0.0" ".2 -all"`,
+		},
+	}))
+	defer dns.HandleRemove("multiline.test.")
+
+	r, e := testResolver.LookupTXTStrict("multiline.test.")
+
+	if e != nil {
+		t.Fatal(e)
+	}
+
+	if len(r) != 1 {
+		t.Errorf("want 1 got %d", len(r))
+	}
+}
+
+func TestMiekgDNSResolver_LookupTXT_Multiline(t *testing.T) {
+	dns.HandleFunc("multiline.test.", zone(map[uint16][]string{
+		dns.TypeTXT: {
+			`multiline.test. 0 IN TXT "v=spf1 ip4:10.0.0.1 ip4:10.0.0" ".2 -all"`,
+		},
+	}))
+	defer dns.HandleRemove("multiline.test.")
+
+	r, e := testResolver.LookupTXT("multiline.test.")
+
+	if e != nil {
+		t.Fatal(e)
+	}
+
+	if len(r) != 1 {
+		t.Errorf("want 1 got %d", len(r))
 	}
 }
